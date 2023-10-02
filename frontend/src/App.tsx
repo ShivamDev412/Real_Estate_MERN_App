2a9da109444df222ef119d53ab2780c245e8b14a
dd <shiv41297@gmail.com>
2023-10-02 01:39:57 -0400

    frontend- setting up routes for pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import ENDPOINTS from "./utils/endpoints";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+const HomePage = React.lazy(() => import("./pages/Home"));
+const SignupPage = React.lazy(() => import("./pages/SignUp"));
+const SigninPage = React.lazy(() => import("./pages/SignIn"));
+const ProfilePage = React.lazy(() => import("./pages/Profile"));
+const AboutPage = React.lazy(() => import("./pages/About"));
 
-function App() {
+function Router() {
+  const { HOME, PROFILE, SIGNUP, SIGNIN, ABOUT } = ENDPOINTS;
   return (
-    <div className='text-3xl text-red-600'>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path={HOME} element={<HomePage />} />
+        <Route path={SIGNIN} element={<SigninPage />} />
+        <Route path={SIGNUP} element={<SignupPage />} />
+        <Route path={PROFILE} element={<ProfilePage />} />
+        <Route path={ABOUT} element={<AboutPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default Router;
---
+++ frontend/src/utils/endpoints.ts
@@ -0,0 +1,8 @@
+const ENDPOINTS = {
+    HOME: '/',
+    PROFILE: '/profile',
+    SIGNUP: '/signup',
+    SIGNIN: '/signin',
+    ABOUT: '/about',
+}
+export default ENDPOINTS;
