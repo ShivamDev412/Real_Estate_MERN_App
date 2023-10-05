b6695efcdce6aff51765562fe65d76795299e3a0
dd <shiv41297@gmail.com>
2023-10-05 07:08:06 -0400

    added redux persist

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,14 @@ import { Provider } from "react-redux";
 import App from "./App.tsx";
 import store from "./redux/store.ts";
 import "./index.css";
-
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./redux/store.ts";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,11 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import rootReducer from "./reducers";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(logger),
   devTools: process.env.NODE_ENV !== "production",
 });
 export type RootState = ReturnType<typeof store.getState>;
+export const persistor = persistStore(store);
 
 export default store;
