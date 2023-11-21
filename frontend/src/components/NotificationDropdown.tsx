e088fae55b0e365154b578ab74c3b51f42a5cbc0
dd <shiv41297@gmail.com>
2023-11-21 16:17:35 -0500

    notification dropdown ui feature edited

--- frontend/src/components/NotificationDropdown.tsx
+++ frontend/src/components/NotificationDropdown.tsx
@@ -118,71 +118,70 @@ const NotificationDropdown = () => {
           <BsFillBellFill className="fill-white h-5 w-5 hover:cursor-pointer" />
         </li>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-80 p-2 max-h-[400px] min-h-[400px] overflow-auto">
-        <section className="flex justify-between items-center mb-2">
-          <h3 className="font-semibold text-[22px] italic">Notifications</h3>
-          {dropDownNotifications?.filter((notification) => !notification.isRead)
-            .length !== 0 && (
-            <button
-              className="hover:text-zinc-600 font-semibold"
-              onClick={markAllAsRead}
-            >
-              Mark All Read
-            </button>
-          )}
-        </section>
-
-        {dropDownNotifications?.length !== 0 &&
-          dropDownNotifications?.map((notification, index: number) => {
-            if (index <= 9)
-              return (
-                <DropdownMenuItem
-                  key={notification._id}
-                  className={`flex gap-1 text-md hover:cursor-pointer my-1 items-center w-full ${
-                    notification.isRead ? "bg-white" : "bg-gray-200"
-                  }`}
-                  onClick={() => {
-                    !notification.isRead && markAsRead(notification._id);
-                    navigate(`/listing-detail/${notification.listingId}`);
-                  }}
-                >
-                  <Avatar>
-                    <AvatarImage
-                      src={notification.profileImage}
-                      alt={`${notification.userName}_image`}
-                    />
-                    <AvatarFallback>
-                      {notification.userName.charAt(0).toLocaleUpperCase()}
-                    </AvatarFallback>
-                  </Avatar>
-                  <section className="flex flex-col w-full">
-                    <h4 className="font-semibold text-lg text-ellipsis overflow-hidden whitespace-nowrap max-w-[90%]">
-                      {notification.title}
-                    </h4>
-                    <p>{notification.message}</p>
-                    <div className="flex justify-between items-center">
-                      <p className="italic text-[12px]">
-                        {notification.isRead ? "read" : "unread"}
-                      </p>
-                      <p className="text-[12px] text-right">
-                        {moment(notification.createdAt).fromNow()}
-                      </p>
-                    </div>
-                  </section>
-                </DropdownMenuItem>
-              );
-          })}
-
-        {totalCount > 9 && (
-          <>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem onClick={() => navigate("/notifications")}>
-              <button className="flex justify-center w-full font-semibold hover:text-zinc-600">
-                Show All Notifications.
+      <DropdownMenuContent className="w-80 p-2 max-h-[400px] min-h-[400px] overflow-auto flex flex-col justify-between">
+        <div>
+          <section className="flex justify-between items-center mb-2">
+            <h3 className="font-semibold text-[22px] italic">Notifications</h3>
+            {dropDownNotifications?.filter(
+              (notification) => !notification.isRead
+            ).length !== 0 && (
+              <button
+                className="hover:text-zinc-600 font-semibold"
+                onClick={markAllAsRead}
+              >
+                Mark All Read
               </button>
-            </DropdownMenuItem>
-          </>
-        )}
+            )}
+          </section>
+
+          {dropDownNotifications?.length !== 0 &&
+            dropDownNotifications?.map((notification, index: number) => {
+              if (index <= 9)
+                return (
+                  <DropdownMenuItem
+                    key={notification?._id}
+                    className={`flex gap-1 text-md hover:cursor-pointer my-1 items-center w-full ${
+                      notification.isRead ? "bg-white" : "bg-gray-200"
+                    }`}
+                    onClick={() => {
+                      !notification.isRead && markAsRead(notification?._id);
+                      navigate(`/listing-detail/${notification.listingId}`);
+                    }}
+                  >
+                    <Avatar>
+                      <AvatarImage
+                        src={notification.profileImage}
+                        alt={`${notification.userName}_image`}
+                      />
+                      <AvatarFallback>
+                        {notification.userName.charAt(0).toLocaleUpperCase()}
+                      </AvatarFallback>
+                    </Avatar>
+                    <section className="flex flex-col w-full">
+                      <h4 className="font-semibold text-lg text-ellipsis overflow-hidden whitespace-nowrap max-w-[90%]">
+                        {notification.title}
+                      </h4>
+                      <p>{notification.message}</p>
+                      <div className="flex justify-between items-center">
+                        <p className="italic text-[12px]">
+                          {notification.isRead ? "read" : "unread"}
+                        </p>
+                        <p className="text-[12px] text-right">
+                          {moment(notification.createdAt).fromNow()}
+                        </p>
+                      </div>
+                    </section>
+                  </DropdownMenuItem>
+                );
+            })}
+        </div>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => navigate("/notifications")}>
+          <button className="flex justify-center w-full font-semibold hover:text-zinc-600">
+            Show All Notifications.
+          </button>
+        </DropdownMenuItem>
+
         {dropDownNotifications?.length === 0 && (
           <NoData title="notifications" styles="" />
         )}
